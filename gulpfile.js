4220d5975d42cf8da64e4ca1ad0bee4e73ef0347
a4927763 <a4927763@gmail.com>
2021-03-25 16:46:06 +0800

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -75,7 +75,7 @@ exports.browser = function browsersync() {
         // browser: "chrome",
         server: {
             baseDir: './dist', //跟目錄設定
-            index: 'act_hist.html', //需更改成自己頁面的名稱
+            index: 'backend.html', //需更改成自己頁面的名稱
             injectChanges: false,
         },
     });
