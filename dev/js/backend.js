337582fcd1ba6c829bb602d3ba292033e31a8e18
a4927763 <a4927763@gmail.com>
2021-03-25 16:17:12 +0800

    add backend

---
+++ dev/js/backend.js
@@ -0,0 +1,18 @@
+
+// 網頁切換
+function openPage(evt, pageState) {
+var i, tabContent, tabLinks;
+tabContent = document.getElementsByClassName("tabContent");
+for (i = 0; i < tabContent.length; i++) {
+tabContent[i].style.display = "none";
+
+}
+tabLinks = document.getElementsByClassName("tabLinks");
+for (i = 0; i < tabLinks.length; i++) {
+tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+}
+document.getElementById(pageState).style.display = "block";
+evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
