0e3337dfcc9a5675cc9adfccde63a44fce446b3f
jsguitar677 <jsguitar677@gmail.com>
2021-04-14 16:38:36 +0800

    no message

--- dev/php/getMemInfo.php
+++ dev/php/getMemInfo.php
@@ -4,6 +4,6 @@ if(isset($_SESSION["MBRMAIL"])===true){ //表示已登入
 	$result =  ["MBRMAIL"=>$_SESSION["MBRMAIL"], "MBRNAME"=>$_SESSION["MBRNAME"]];
 	echo json_encode($result);
 }else{
-	echo "{}";
+	echo 50;
 }
 ?>
