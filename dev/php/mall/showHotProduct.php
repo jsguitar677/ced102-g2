a3b649f9066dc9d5be68178e167e261baa06b2cd
KAY <kaywu5577@gmail.com>
2021-04-19 19:04:48 +0800

    update html & add php of mall

---
+++ dev/php/mall/showHotProduct.php
@@ -0,0 +1,10 @@
+<?php 
+try {
+	require_once("../../connect_ced102g2.php");
+	$sql = "select * from prod where prodca = 1 group by prodclno order by prodclno desc limit 3";
+	$NewProuct = $pdo->query($sql);
+	$NewProdRows = $NewProuct->fetchAll(PDO::FETCH_ASSOC);
+	echo json_encode($NewProdRows);
+} catch (PDOException $e) {
+}
+?>
