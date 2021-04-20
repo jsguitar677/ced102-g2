74f22c05759fd478147e93364b20ef8f3a8becbc
JohnLing73 <dudulala73@gmail.com>
2021-04-20 21:33:10 +0800

    首頁數字(捐款,人力)撈資料

--- dev/php/2/indexfront.php
+++ dev/php/2/indexfront.php
@@ -1,3 +1,23 @@
 <?php
-require_once("./connectBooks.php");
+try{
+    require_once("./connectBooks.php");
+    //抓取actv table 中的  DNTNOW RECRNOW 整列的ROW數 for 發起活動場次
+    $sql = "select DNTNOW, RECRNOW from actv";
+    $actv = $pdo->query($sql);
+    $actvRows = $actv->fetchAll(PDO::FETCH_ASSOC);
+    // print_r($actvRows);
+    $donationTot = 0;
+    $recruitTot = 0;
+    $result = array();
+    foreach($actvRows as $i => $actvRow){
+            $donationTot = $donationTot + $actvRows[$i]["DNTNOW"];
+            $recruitTot = $recruitTot + $actvRows[$i]["RECRNOW"];
+    }
+    $actvTot = count($actvRows);
+    $return = ["donationTot" => $donationTot, "recruitTot"=>$recruitTot, "actvTot"=>$actvTot];
+    echo json_encode($return);
+}catch(PDOException $e){
+    echo $e->getMessage();
+}
+
 ?>
