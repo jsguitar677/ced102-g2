d828e56602d419d238d62477f966fdf143f9e22a
a4927763 <a4927763@gmail.com>
2021-04-25 00:32:40 +0800

    no message

--- dev/connect_ced102g2.php
+++ dev/connect_ced102g2.php
@@ -1,10 +1,10 @@
 <?php
-    // $dbname = 'tebamefe_ced102g2';
-    // $user = 'tibamefe_since2021';
-    // $password = 'vwRBSb.j&K#E';
-    $dbname = 'jingshafinal';
-    $user = 'root';
-    $password = 'root';
+    $dbname = 'tebamefe_ced102g2';
+    $user = 'tibamefe_since2021';
+    $password = 'vwRBSb.j&K#E';
+    // $dbname = '淨下塑源';
+    // $user = 'root';
+    // $password = '11111';
     $dsn = "mysql:host=localhost;port=3306;dbname=$dbname;charset=utf8";
     $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION , //出錯要提醒
                     PDO::ATTR_CASE => PDO::CASE_NATURAL); //欄位照MySQL原本大小寫
