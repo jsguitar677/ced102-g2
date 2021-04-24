513f097a66affbf06b5514e26cb890ee3307fe6f
JohnLing73 <dudulala73@gmail.com>
2021-04-24 16:52:24 +0800

    最新活動修正

--- dev/php/2/indexfront_latestAct.php
+++ dev/php/2/indexfront_latestAct.php
@@ -4,7 +4,7 @@ session_start();
         // require_once("./connectBooks.php");
         require_once("../../connect_ced102g2.php");
         //LOCATION LOCPIC;
-        $sql = "select a.ACTNO, a.ACTLOC, a.ACTNAME, a.RECRGOAL, a.RECRNOW, a.DNTGOAL, a.DNTNOW, a.ACTDLINE, b.LOCPIC from ACTV a JOIN LOCATION b ON a.ACTLOC = b.LOCNAME order by a.ACTSDATE desc";
+        $sql = "select a.ACTNO, a.ACTLOC, a.ACTNAME, a.RECRGOAL, a.RECRNOW, a.DNTGOAL, a.DNTNOW, a.ACTDLINE, a.ACTSTAT, b.LOCPIC from ACTV a JOIN LOCATION b ON a.ACTLOC = b.LOCNAME where a.ACTSTAT = 2 or a.ACTSTAT = 3 or a.ACTSTAT = 5 order by a.ACTSDATE desc";
         $LatestActvRow = $pdo->query($sql);
         $LatestActvRows = $LatestActvRow->fetchAll(PDO::FETCH_ASSOC);
         // $_SESSION["ACTNAME"] = $LatestActvRows["ACTNAME"];
