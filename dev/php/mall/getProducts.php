65553e315f97c14f6101f940e14b4e2a35d3c61f
KAY <kaywu5577@gmail.com>
2021-04-25 16:27:11 +0800

    no message

--- dev/php/mall/getProducts.php
+++ dev/php/mall/getProducts.php
@@ -1,7 +1,7 @@
 <?php 
 try {
 	require_once("../../connect_ced102g2.php");
-	$sql = "select * from prod where prodca = 1";
+	$sql = "select * from prod where (prodca=1 and prodstat=1) order by prodno";
 	$products = $pdo->query($sql);
 	$prodRows = $products->fetchAll(PDO::FETCH_ASSOC);
 	echo json_encode($prodRows);
